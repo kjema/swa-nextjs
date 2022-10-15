697c4428dafacc6302a0d6df4e4179d901c46abd
Marcus Kjell <marcus.kjell@gmail.com>
2022-10-15 18:34:57 +0200

    Playwright updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,21 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-const config: PlaywrightTestConfig = {
-    use: {
-        trace: 'on',
-        baseURL: "http://localhost:3000/"
-    },
-    webServer: {
-        command: 'npm run start',
-        port: 3000,
-        timeout: 120 * 1000,
-        reuseExistingServer: !process.env.CI,
-    },
-    reporter: [
-        ['html', { outputFolder: 'pw-report' }], 
-        ['json', { outputFolder: 'pw-report', outputFile: 'report.json' }]
-    ],
+import { PlaywrightTestConfig } from "@playwright/test";
 
+const config: PlaywrightTestConfig = {
+  testDir: "./src/tests",
+  use: {
+    trace: "on",
+    baseURL: "http://localhost:3000/",
+  },
+  webServer: {
+    command: "npm run start",
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ["html", { outputFolder: "pw-report" }],
+    ["json", { outputFolder: "pw-report", outputFile: "report.json" }],
+  ],
 };
 export default config;
